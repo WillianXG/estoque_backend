815f1e9360b663bd8f4e96a7c393702b88794780
WillianXG <wilian.mine789@gmail.com>
2026-02-14 20:16:02 -0300

    Ajuste Imagem

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use("/vendas", vendasRoutes);
 app.use("/estoque", estoqueMovimentacaoRoutes);
 app.use("/movimentacoes-estoque", estoqueMovimentacaoRoutes); 
 app.use("/vendedoras", vendedorasRoutes);
-app.use("/uploads", express.static(path.resolve("uploads")));
+app.use("/uploads", express.static("uploads"));
 app.get("/", (req, res) =>
     {
         res.json({ ok: true });
