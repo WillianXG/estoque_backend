6d11cdf6795d37f1fe94012e84fb139cc1dde623
WillianXG <wilian.mine789@gmail.com>
2026-03-15 15:34:09 -0300

    AJuste tamanho imamge

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -13,7 +13,7 @@ const router = Router();
 
 const upload = multer({
   storage: multer.memoryStorage(),
-  limits: { fileSize: 5 * 1024 * 1024 },
+  limits: { fileSize: 15 * 1024 * 1024 },
 });
 
 /* =========================
